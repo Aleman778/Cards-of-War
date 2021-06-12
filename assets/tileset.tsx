eb0873f98035f0e5b30e61d9affeffcfb1357843
FE3000 <fadi.eliasson.97@gmail.com>
2021-06-12 15:31:35 +0200

    Added the tsx file for Tiled

---
+++ assets/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="tileset" tilewidth="16" tileheight="16" tilecount="81" columns="9">
+ <image source="misc_tilset.png" width="144" height="144"/>
+</tileset>
